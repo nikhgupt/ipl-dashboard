6262ec2d2c2a4a46519abe1939b882cb95bb86c9
Nikhil Gupta <nikhilgupta.se@gmail.com>
2021-05-02 22:49:33 +0530

    React app with Team Page

--- src/main/java/com/javaproject/ipldashboard/controller/TeamController.java
+++ src/main/java/com/javaproject/ipldashboard/controller/TeamController.java
@@ -4,11 +4,13 @@ import com.javaproject.ipldashboard.model.Team;
 import com.javaproject.ipldashboard.repository.MatchRepository;
 import com.javaproject.ipldashboard.repository.TeamRepository;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
