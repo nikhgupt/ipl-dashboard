bdc8229e7d93f3cdfdb4cb040fc40047a9768353
Nikhil Gupta <nikhilgupta.se@gmail.com>
2021-05-29 21:44:49 +0530

    Fixed warnings

--- src/frontend/src/pages/HomePage.js
+++ src/frontend/src/pages/HomePage.js
@@ -1,8 +1,4 @@
 import { React, useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import { MatchDetailCard } from '../components/MatchDetailCard';
-import { MatchSmallCard } from '../components/MatchSmallCard';
-import { PieChart } from 'react-minimal-pie-chart';
 
 
 import './HomePage.scss';
--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,7 +1,6 @@
 import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
-import { MatchSmallCard } from '../components/MatchSmallCard';
 import { YearSelector } from '../components/YearSelector';
 
 import './MatchPage.scss';
